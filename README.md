bbf99d90e932ab4f6fcd9bc157f2226fd1dec5b5
Jose Alberto Leyva Contreras <josealc1998@gmail.com>
2024-06-30 21:20:34 -0700

    Initial Commit - Clase 1 y README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Curso Platzi JS desde Cero
+
+Repositorio con archivos y proyectos del curso de Platzi
+
+### Link del Curso
+
+Puedes ver el link del curso aquí: [Link](https://platzi.com/home/clases/8617-javascript-fundamentos/)
---
+++ clase-variables.js
@@ -0,0 +1,15 @@
+let cajaDeAndy = "Woody"
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = "Woody"
+let cda = "Woody"
+let pcAndy = "Woody"
+
+// let c = "Buzz"  --> volver a declarar variables
+
+
+// Lo permitido
+let PrimerJugeteAndy = "Woody"
+let URLDeUsuario = "https://google.com"
+let IDUsuario = 6104
